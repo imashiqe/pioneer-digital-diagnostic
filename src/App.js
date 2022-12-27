2e512fd230435420d5ef0e815c5501a35226dd92
imashiqe <imashiqe@gmail.com>
2022-12-27 14:10:31 +0600

    bg color fix and  header added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h3>Hello  Pioneer Digital  Diagnostic</h3>
-    </div>
+      <div>
+         <Header></Header>
+      </div>
   );
 }
 
