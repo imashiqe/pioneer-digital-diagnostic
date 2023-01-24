019808e417eab7a54b463b8c1745f8eca0d32431
imashiqe <imashiqe@gmail.com>
2023-01-25 01:26:57 +0600

    path added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,31 +1,10 @@
 import React from 'react';
+import Banner from './Banner/Banner';
 
 const Home = () => {
     return (
-        <div>
-            {/* banner part start */}
-            <div className="carousel w-full">
-  <div id="item1" className="carousel-item w-full">
-    <img src="https://placeimg.com/800/200/arch" className="w-full" />
-  </div> 
-  <div id="item2" className="carousel-item w-full">
-    <img src="https://placeimg.com/800/200/arch" className="w-full" />
-  </div> 
-  <div id="item3" className="carousel-item w-full">
-    <img src="https://placeimg.com/800/200/arch" className="w-full" />
-  </div> 
-  <div id="item4" className="carousel-item w-full">
-    <img src="https://placeimg.com/800/200/arch" className="w-full" />
-  </div>
-</div> 
-<div className="flex justify-center w-full py-2 gap-2">
-  <a href="#item1" className="btn btn-xs">1</a> 
-  <a href="#item2" className="btn btn-xs">2</a> 
-  <a href="#item3" className="btn btn-xs">3</a> 
-  <a href="#item4" className="btn btn-xs">4</a>
-</div>
-
- {/* banner part end */}
+        <div className='py-12'>
+         <Banner></Banner>
         </div>
     );
 };
