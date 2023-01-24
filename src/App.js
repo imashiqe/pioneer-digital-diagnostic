67d8fd6f81fb074cb5106b8b503aa7ec957119ba
imashiqe <imashiqe@gmail.com>
2023-01-25 00:36:15 +0600

    react router config

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
+import { RouterProvider } from 'react-router-dom';
 
 function App() {
   return (
       <div>
-         <Header></Header>
+        <RouterProvider></RouterProvider>
       </div>
   );
 }
---
+++ src/Routes/Routes/Routes.js
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../../layout/Main";
+
+export const routes = createBrowserRouter([
+    {
+      path:'/',
+      element: <Main></Main>,
+      children: [
+         {
+
+         }
+      ]
+    },
+
+
+]);
