7c97e16a2c8b674f092b6c009ba6e9ae840b89fd
imashiqe <imashiqe@gmail.com>
2023-01-25 23:40:47 +0600

    service part added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from './Banner/Banner';
+import Services from './Services/Services';
 
 const Home = () => {
     return (
         <div className='py-12'>
          <Banner></Banner>
+         <Services></Services>
         </div>
     );
 };
---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <div className='container mx-auto'>
+            <h3 className='text-center text-4xl py-10 text-shadow'>Our Service</h3>
+
+            <div class="grid grid-cols-1  lg:grid-cols-3      gap-4">
+           
+            <div className="card w-96 bg-base-100 shadow-xl">
+            <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+     <div className="card-body">
+    <h2 className="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+     </div>
+     </div>
+   </div>
+            <div className="card w-96 bg-base-100 shadow-xl">
+            <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+     <div className="card-body">
+    <h2 className="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+     </div>
+     </div>
+   </div>
+            <div className="card w-96 bg-base-100 shadow-xl">
+            <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+     <div className="card-body">
+    <h2 className="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+     </div>
+     </div>
+   </div>
+
+          
+
+           
+
+
+
+
+           </div>
+
+        </div>
+    );
+};
+
+export default Services;
