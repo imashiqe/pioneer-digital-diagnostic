417daeab82e73c107aaff93ddc5497225c14f9b9
imashiqe <imashiqe@gmail.com>
2023-01-27 23:14:12 +0600

    menu  hover  update  and color  change  padding

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,44 +11,49 @@ const Header = () => {
       <label tabIndex={0} className="btn btn-ghost lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="red" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </label>
-      <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-emerald-800 rounded-box w-52">
+      <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-green-300 rounded-box w-52">
         <Link to="/">Home</Link>
         <Link to="HealthCard">HealthCard</Link>
-         
+        <Link to='training'>Training</Link>
+        <Link to='contact'>Contact Us</Link>
      
       </ul>
     </div>
-      <img className='logo' src={logo} />
+      <Link to='/'><img className='logo' src={logo} /></Link>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
-    <Link to="/">Home</Link>
+    <Link to="/" className='pt-3'>Home</Link>
     
       <li><a>Doctors</a></li>
-      <Link to="HealthCard">HealthCard</Link>
+      <Link to="HealthCard" className='pt-3'>HealthCard</Link>
       <li tabIndex={0}>
         <a>
          Our Services
           <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
         </a>
-        <ul className="p-2">
-          <li><a>Submenu 1</a></li>
-          <li><a>Submenu 2</a></li>
+        <ul className="p-2 bg-green-300">
+          <Link className='hover:text-green-600'>All Services</Link>
+          <Link className='hover:text-green-600'>Medical Lab Test</Link>
+         <Link className='hover:text-green-600'>Vaccine Prices</Link>
         </ul>
       </li>
-      <Link to='training'>Training</Link>
+      <Link to='training' className='pt-3'>Training</Link>
       <li tabIndex={0}>
         <a>
          About Us
           <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
         </a>
-        <ul className="p-2">
-          <li><a>Submenu 1</a></li>
-          <li><a>Submenu 2</a></li>
+        <ul className="p-2 bg-green-300">
+          <Link className='hover:text-green-600'>Our Speech</Link>
+          <Link className='hover:text-green-600'>Company Profile</Link>
+          <Link className='hover:text-green-600'>Legal Papers</Link>
+
+          
         </ul>
       </li>
 
-      <Link to='contact'>Contact Us</Link>
+      <Link to='contact' className='pt-3'>Contact Us</Link>
     </ul>
   </div>
   <div className="navbar-end">
