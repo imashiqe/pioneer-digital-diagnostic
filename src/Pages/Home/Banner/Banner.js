1c898359520b4d24e6a72d30f50c5229390c3024
imashiqe <imashiqe@gmail.com>
2023-01-27 20:06:13 +0600

    banner  image updated

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -6,26 +6,36 @@ import  imgfour from "../../../assets/image/Have The Right Jone.jpg"
 import "./Banner.css";
 const Banner = () => {
     return (
-        <div className='pt-10 shadow-xl'>
+        <div className='pt-10 shadow-xl border-b-2 border-emerald-500'>
                         <div className="carousel w-full">
-  <div id="item1" className="carousel-item w-full">
-    <img src={imgone} className="w-full imgcustom" />
+  <div id="slide1" className="carousel-item relative w-full">
+    <img src={imgone} className="w-full" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide4" className="btn btn-circle">❮</a> 
+      <a href="#slide2" className="btn btn-circle">❯</a>
+    </div>
   </div> 
-  <div id="item2" className="carousel-item w-full">
-    <img src={imgtwo} className="w-full imgcustom" />
+  <div id="slide2" className="carousel-item relative w-full">
+    <img src={imgtwo} className="w-full" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide1" className="btn btn-circle">❮</a> 
+      <a href="#slide3" className="btn btn-circle">❯</a>
+    </div>
   </div> 
-  <div id="item3" className="carousel-item w-full">
-    <img src={imgthree} className="w-full imgcustom" />
+  <div id="slide3" className="carousel-item relative w-full">
+    <img src={imgthree} className="w-full" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide2" className="btn btn-circle">❮</a> 
+      <a href="#slide4" className="btn btn-circle">❯</a>
+    </div>
   </div> 
-  <div id="item4" className="carousel-item w-full">
-    <img src={imgfour} className="w-full imgcustom" />
+  <div id="slide4" className="carousel-item relative w-full">
+    <img src={imgfour} className="w-full" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide3" className="btn btn-circle">❮</a> 
+      <a href="#slide1" className="btn btn-circle">❯</a>
+    </div>
   </div>
-</div> 
-<div className="flex justify-center w-full py-2 gap-2">
-  <a href="#item1" className="btn btn-xs">1</a> 
-  <a href="#item2" className="btn btn-xs">2</a> 
-  <a href="#item3" className="btn btn-xs">3</a> 
-  <a href="#item4" className="btn btn-xs">4</a>
 </div>
         </div>
     );
